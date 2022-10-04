b59a1ab6cc47ccc94ed7c8b80b16dd0a010ec28c
Edgar Ghazaryan <edgghaza@c2r11s3.42yerevan.am>
2022-10-04 22:11:12 +0400

    fixed case with HOME

--- ft_cd.c
+++ ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: edgghaza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 17:50:38 by edgghaza          #+#    #+#             */
-/*   Updated: 2022/10/04 21:34:20 by edgghaza         ###   ########.fr       */
+/*   Updated: 2022/10/04 22:04:59 by edgghaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,17 @@ int	call_cd(char *prompt, t_env *env)
 	splited_prompt = ft_split(prompt, ' ');
 	if (!splited_prompt[1])
 	{
-		if (chdir(_getenv(env, "HOME")) == -1)
+		if (!exists_key("HOME", env))
 		{
 			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
+			free_after_split(splited_prompt);				
+			return (1);	
+		}
+		else if (chdir(_getenv(env, "HOME")) == -1)
+		{
+			ft_putstr_fd("minishell: cd: ", 2);
+			ft_putstr_fd(_getenv(env, "HOME"), 2);
+			ft_putstr_fd(": No such file or directory\n", 2);
 			free_after_split(splited_prompt);				
 			return (1);
 		}
