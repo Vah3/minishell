700cfd54aacae1aedb4f8cb47086b2e7b4241bed
Vahe Gevorgyan <vagevorg@c1r3s3.42yerevan.am>
2022-10-10 20:00:08 +0400

    norm

--- processes.c
+++ processes.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   processes.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edgghaza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vagevorg <vagevorg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 14:44:28 by vagevorg          #+#    #+#             */
-/*   Updated: 2022/10/10 17:27:16 by edgghaza         ###   ########.fr       */
+/*   Updated: 2022/10/10 19:45:26 by vagevorg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-extern int g_status;
+extern int	g_status;
 
 int	close_pipes(int (*fd)[2], int count)
 {
@@ -41,6 +41,7 @@ void	malloc_and_check(int count, int ***fd_, t_pars **pars, pid_t **id_)
 	pid_t	*id;
 
 	id = NULL;
+	*id_ = NULL;
 	if (init_pipe(fd_, count))
 	{
 		free_pars(pars);
@@ -76,7 +77,7 @@ int	do_fork(pid_t **id, int i)
 		{
 			if (kill((*id)[i_], SIGKILL))
 				perror("kill");
-			if(wait(NULL) < 0)
+			if (wait(NULL) < 0)
 				perror("");
 			i_++;
 		}
@@ -86,16 +87,16 @@ int	do_fork(pid_t **id, int i)
 	return (SUCCESS);
 }
 
-
-void	print_in_errno_and_free_exit(char **command, char *print, int code , char **cmd)
+void	print_in_errno_and_free_exit(
+	char **command, char *print, int code, char **cmd)
 {
-	int 	i;
+	int	i;
 
 	i = 0;
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(command[0], 2);
 	ft_putendl_fd(print, 2);
-	while(command && command[i])
+	while (command && command[i])
 	{
 		free(command[i]);
 		free(cmd[i]);
@@ -106,77 +107,73 @@ void	print_in_errno_and_free_exit(char **command, char *print, int code , char *
 	exit(code);
 }
 
-typedef struct for_free
-{
-	int		(*fd)[2];
-	pid_t	*id;
-	t_pars **pars;
-} t_free;
-
 void	do_execve(t_pars *pars, char **env, t_env *env_)
 {
 	char	**cmd;
 	char	*line;
 	char	**command;
-	// char	*cr_cwd;
 
-	// cr_cwd = getcwd(NULL, 0);
 	cmd = pars->exec_cmd;
 	line = pars->cmd;
 	line = get_correct_cmd(line);
 	command = ft_split(line, 32);
 	if (there_is_builtin(line))
-		exit( call_builtin(&pars, line, there_is_builtin(line), env_));
-	if(cmd && cmd[0])
+		exit(call_builtin(&pars, line, there_is_builtin(line), env_));
+	if (cmd && cmd[0])
 		execve(cmd[0], cmd, env);
 	if (cmd && cmd[0] && opendir(cmd[0]))
 		print_in_errno_and_free_exit(command, " is a directory", 126, cmd);
 	if (cmd && !cmd[0] && ft_strchr(command[0], '/'))
-		print_in_errno_and_free_exit(command, " No such file or directory", 127, cmd);
+		print_in_errno_and_free_exit
+		(command, " No such file or directory", 127, cmd);
 	else if (cmd && (!cmd[0] || !ft_strchr(command[0], '/')))
-		print_in_errno_and_free_exit(command, " Command not found", 127, cmd);
-	if (cmd && cmd[0] && access(cmd[0], F_OK) == 0 && access(cmd[0], X_OK) == -1)
+		print_in_errno_and_free_exit
+			(command, " Command not found", 127, cmd);
+	if (cmd && cmd[0] && access(cmd[0], F_OK) == 0
+		&& access(cmd[0], X_OK) == -1)
 		print_in_errno_and_free_exit(command, " Permission denied", 126, cmd);
-	// if (!cr_cwd)
-	// 	printf("feyyyl\n");
 	exit(g_status);
 }
 
+void	sig_and_set_attr(pid_t *id, int i)
+{
+	struct termios	a;
 
+	if (id[i] == 0)
+	{
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
+		tcgetattr(0, &a);
+		a.c_lflag |= ECHOCTL;
+		tcsetattr(0, 0, &a);
+	}
+}
+
+int	free_and_close(int (*fd)[2], int count, t_pars **pars, pid_t *id)
+{
+	close_pipes(fd, count);
+	fr(pars, fd, id, count);
+	return (1);
+}
 
 void	open_processes(int count, t_pars **pars, char **env, t_env *env_)
 {
 	int		i;
 	int		(*fd)[2];
-	pid_t	*id = NULL;
-	struct termios a;
+	pid_t	*id;
 
 	i = -1;
-
 	malloc_and_check(count, (int ***)&fd, pars, &id);
 	while (++i < count)
 	{
-		if(do_fork(&id, i))
-		{
-			close_pipes(fd, count);
-			fr(pars, fd, id, count);
+		if (do_fork(&id, i) && free_and_close(fd, count, pars, id))
 			return ;
-		}
+		sig_and_set_attr(id, i);
 		if (id[i] == 0)
-		{
-			signal(SIGINT, SIG_DFL);
-			signal(SIGQUIT, SIG_DFL);
-			tcgetattr(0, &a);
-			a.c_lflag |= ECHOCTL;
-			tcsetattr(0, 0, &a);
-		}
-		if (id[i] == 0 )
-		 	make_cmd(pars[i], env);
-		if (id[i] == 0 && count == 2 && single_pipe(i, fd, pars[i])
-			&& fr(pars, fd, id, count))
+			make_cmd(pars[i], env);
+		if (id[i] == 0 && count == 2 && single_pipe(i, fd, pars[i]))
 			exit (EXIT_FAILURE);
-		if (id[i] == 0 && count > 2 && multi_pipe(i, fd, count, pars[i])
-			&& fr(pars, fd, id, count))
+		if (id[i] == 0 && count > 2 && multi_pipe(i, fd, count, pars[i]))
 			exit (EXIT_FAILURE);
 		if (id[i] == 0 && count == 1)
 			without_pipes(pars, fd, id, count);
